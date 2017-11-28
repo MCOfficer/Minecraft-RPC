20cda07cb4001bd04a9b48838a0784073703d135
M*C*O <22377202+MCOfficer@users.noreply.github.com>
2017-11-28 14:44:10 +0100

    readying for 1.1

--- src/main/java/me/mcofficer/minecraftrpc/MinecraftRPC.java
+++ src/main/java/me/mcofficer/minecraftrpc/MinecraftRPC.java
@@ -13,7 +13,7 @@ public class MinecraftRPC {
 
     public static final String modId = "minecraftrpc";
     public static final String name = "Minecraft-RPC";
-    public static final String version = "1.0.0";
+    public static final String version = "1.1";
 
     public EventHandler eventHandler = new EventHandler(this);
     public RPCClient rpcClient;
